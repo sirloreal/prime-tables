2ca40c21ff247f6045c80a2edf7e34e41e7b1476
sirloreal <gary_page@garypage.net>
2016-04-27 12:02:55 +0100

    Implementation of naive prime generator

--- PrimeTables/NumberAlgorithms/PrimeGenerator.cs
+++ PrimeTables/NumberAlgorithms/PrimeGenerator.cs
@@ -31,11 +31,39 @@ namespace PrimeTables.NumberAlgorithms
                 throw new ArgumentException("Number of primes must be greater than one");
             }
             
-            //2 and 3 are known to be prime
+            /**
+             * 2 and three are known to be prime. We can add
+             * 2 for free since we know the requested number of primes
+             * is at least 1. Then, we can run the algorithm below starting from
+             * the number 3
+             */            
             results.Add(2);
-            if (numberOfPrimes.Value > 1)
+            int nextPrime = 3;
+
+            //Inspiration for initial algorithm sourced from stackoverflow:
+            //http://stackoverflow.com/questions/1042902/most-elegant-way-to-generate-prime-numbers
+            while (results.Count < numberOfPrimes.Value)
             {
-                results.Add(3);
+                bool isPrime = true;            
+        
+                //TODO: Are there any possible pitfalls or missing numbers when using the results set for dividing?
+                foreach (int n in results)
+                {
+                    if (nextPrime % n == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    results.Add(nextPrime);
+                }
+
+                //Get the next candidate number. Increase candidacy by 2 each
+                //time since we know even numbers (save 2) will NOT be prime
+                nextPrime += 2;
             }
 
             return results.ToArray();
